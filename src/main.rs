b689ded92858f4d7a2826432f1fd4d2e8867ba0d
Jonny Dee <jonny.dee@gmx.net>
2015-08-08 22:01:50 +0200

    Add support for pixel again.

--- src/main.rs
+++ src/main.rs
@@ -3,65 +3,31 @@ extern crate image;
 
 use bit_vec::BitVec;
 use image::{ GenericImage, ImageBuffer };
-use std::fs::File;
 
 
-struct Pixel {
+struct Canvas<'a> {
     width : usize,
     height : usize,
-    buffer : Vec<char>,
+    pixmap : &'a Pixmap,
+    pixel : &'a Pixmap,
+    empty_pixel : Pixmap,
 }
 
-impl Pixel {
-    fn new(width : usize, height : usize, dot : char) -> Pixel {
-        let size = width*height;
-        Pixel {
+impl<'a> Canvas<'a> {
+    fn new(pixmap : &'a Pixmap, pixel : &'a Pixmap) -> Canvas<'a> {
+        let empty_pixel = Pixmap::new(pixel.width, pixel.height, false);
+        let width = pixmap.width * pixel.width;
+        let height = pixmap.height * pixel.height;
+        Canvas {
             width : width,
             height : height,
-            buffer : vec![dot; size],
-        }
-    }
-
-    fn new_empty(width : usize, height : usize) -> Pixel {
-        Pixel::new(width, height, ' ')
-    }
-    
-    fn set_dot(&mut self, x : usize, y : usize, dot : char) {
-        let offset = y*self.width + x;
-        self.buffer[offset] = dot;
-    }
-    
-    fn get_dot(&self, x : usize, y : usize) -> char {
-        let offset = y*self.width + x;
-        self.buffer[offset]
-    }
-}
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
-
-struct Canvas {
-    pixmap : Pixmap,
-    pixel : Pixel,
-    empty_pixel : Pixel,
-}
-
-impl Canvas {
-    fn new(pixmap : Pixmap, pixel : Pixel) -> Canvas {
-        let empty_pixel = Pixel::new_empty(pixel.width, pixel.height);
-        Canvas {
-            pixmap : pixmap,
-            pixel : pixel,
+            pixmap : &pixmap,
+            pixel : &pixel,
             empty_pixel : empty_pixel,
         }
     }
     
-    fn get_charmap_size(&self) -> (usize, usize) {
-        let width = self.pixmap.width * self.pixel.width;
-        let height = self.pixmap.height * self.pixel.height;
-        (width, height)
-    }
-    
-    fn get_charmap_dot(&self, x : usize, y : usize) -> char {
+    fn get(&self, x : usize, y : usize) -> bool {
         let pixel_at_x = x / self.pixel.width;
         let pixel_at_y = y / self.pixel.height;
         
@@ -69,21 +35,22 @@ impl Canvas {
         let pixel_y = y % self.pixel.height;
 
         let dot = self.pixmap.get(pixel_at_x, pixel_at_y);
-        let pixel = if dot { &self.pixel } else { &self.empty_pixel };
-        pixel.get_dot(pixel_x, pixel_y)
+        let pixel = if dot { self.pixel } else { &self.empty_pixel };
+        pixel.get(pixel_x, pixel_y)
     }
     
     fn print(&self) {
-        let (width, height) = self.get_charmap_size();        
-        for y in 0..height {
-            for x in 0..width {
-                let dot = self.get_charmap_dot(x, y);
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let dot = if self.get(x, y) { '*' } else { ' ' };
                 print!("{}", dot);
             }
             println!("");
         }
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct Pixmap {
     width : usize,
@@ -92,11 +59,11 @@ struct Pixmap {
 }
 
 impl Pixmap {
-    fn new(width : usize, height : usize) -> Pixmap {
+    fn new(width : usize, height : usize, filled : bool) -> Pixmap {
         Pixmap {
             width : width,
             height : height,
-            buffer : BitVec::from_elem(width*height, false),
+            buffer : BitVec::from_elem(width*height, filled),
         }
     }
     
@@ -111,18 +78,20 @@ impl Pixmap {
     }
 }
 
-struct RecursiveImage {
-    icon : Pixmap,
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct RecursiveImage<'a> {
+    brush : &'a Pixmap,
     buffer : Pixmap,
 }
 
-impl RecursiveImage {
-    fn draw(icon : Pixmap, depth : usize) -> Pixmap {
-        let width = icon.width.pow(depth as u32);
-        let height = icon.height.pow(depth as u32);
-        let buffer = Pixmap::new(width, height);
+impl<'a> RecursiveImage<'a> {
+    fn draw(brush : &'a Pixmap, depth : usize) -> Pixmap {
+        let width = brush.width.pow(depth as u32);
+        let height = brush.height.pow(depth as u32);
+        let buffer = Pixmap::new(width, height, false);
         let mut image = RecursiveImage {
-            icon : icon,
+            brush : brush,
             buffer : buffer,
         };
         image.draw_pixmap(depth, 0, 0);
@@ -131,22 +100,22 @@ impl RecursiveImage {
     
     fn draw_pixmap(&mut self, depth : usize, row : usize, col : usize) {
         if 1 == depth {
-            for y in 0..(self.icon.height) {
-                for x in 0..(self.icon.width) {
-                    let draw = self.icon.get(x, y);
+            for y in 0..(self.brush.height) {
+                for x in 0..(self.brush.width) {
+                    let draw = self.brush.get(x, y);
                     if draw { 
                         self.buffer.set(col + x, row + y, true);
                     }
                 }
             }
         } else {
-            let width = self.icon.width.pow(depth as u32);
-            let height = self.icon.height.pow(depth as u32);
-            let x_step = width / self.icon.width;
-            let y_step = height / self.icon.height;
-            for y in 0..(self.icon.height) {
-                for x in 0..(self.icon.width) {
-                    let draw = self.icon.get(x, y);
+            let width = self.brush.width.pow(depth as u32);
+            let height = self.brush.height.pow(depth as u32);
+            let x_step = width / self.brush.width;
+            let y_step = height / self.brush.height;
+            for y in 0..(self.brush.height) {
+                for x in 0..(self.brush.width) {
+                    let draw = self.brush.get(x, y);
                     if draw {
                         self.draw_pixmap(depth - 1, row + y * y_step, col + x * x_step);
                     }
@@ -156,23 +125,27 @@ impl RecursiveImage {
     }
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 fn main() {
-    let icon = {
-        let mut i = Pixmap::new(7, 7);
-        i.set(0, 0, false); i.set(1, 0, false); i.set(2, 0, false); i.set(3, 0, false); i.set(4, 0, false); i.set(5, 0, false); i.set(6, 0, false);
-        i.set(0, 1, true); i.set(1, 1, true); i.set(2, 1, false); i.set(3, 1, false); i.set(4, 1, true); i.set(5, 1, true); i.set(6, 1, true);
-        i.set(0, 2, true); i.set(1, 2, false); i.set(2, 2, true); i.set(3, 2, false); i.set(4, 2, false); i.set(5, 2, false); i.set(6, 2, true);
-        i.set(0, 3, true); i.set(1, 3, false); i.set(2, 3, true); i.set(3, 3, false); i.set(4, 3, false); i.set(5, 3, false); i.set(6, 3, true);
-        i.set(0, 4, true); i.set(1, 4, false); i.set(2, 4, true); i.set(3, 4, false); i.set(4, 4, true); i.set(5, 4, false); i.set(6, 4, true);
-        i.set(0, 5, true); i.set(1, 5, true); i.set(2, 5, false); i.set(3, 5, false); i.set(4, 5, false); i.set(5, 5, true); i.set(6, 5, false);
-        i.set(0, 6, false); i.set(1, 6, false); i.set(2, 6, false); i.set(3, 6, false); i.set(4, 6, false); i.set(5, 6, false); i.set(6, 6, false);
-        i
+    let brush = {
+        let mut b = Pixmap::new(7, 7, false);
+        b.set(0, 0, false); b.set(1, 0, false); b.set(2, 0, false); b.set(3, 0, false); b.set(4, 0, false); b.set(5, 0, false); b.set(6, 0, false);
+        b.set(0, 1, true); b.set(1, 1, true); b.set(2, 1, false); b.set(3, 1, false); b.set(4, 1, true); b.set(5, 1, true); b.set(6, 1, true);
+        b.set(0, 2, true); b.set(1, 2, false); b.set(2, 2, true); b.set(3, 2, false); b.set(4, 2, false); b.set(5, 2, false); b.set(6, 2, true);
+        b.set(0, 3, true); b.set(1, 3, false); b.set(2, 3, true); b.set(3, 3, false); b.set(4, 3, false); b.set(5, 3, false); b.set(6, 3, true);
+        b.set(0, 4, true); b.set(1, 4, false); b.set(2, 4, true); b.set(3, 4, false); b.set(4, 4, true); b.set(5, 4, false); b.set(6, 4, true);
+        b.set(0, 5, true); b.set(1, 5, true); b.set(2, 5, false); b.set(3, 5, false); b.set(4, 5, false); b.set(5, 5, true); b.set(6, 5, false);
+        b.set(0, 6, false); b.set(1, 6, false); b.set(2, 6, false); b.set(3, 6, false); b.set(4, 6, false); b.set(5, 6, false); b.set(6, 6, false);
+        b
     };
-        
-    let pixmap = RecursiveImage::draw(icon, 5);
     
-    let img = ImageBuffer::from_fn(pixmap.width as u32, pixmap.height as u32, |x, y| {
-        if pixmap.get(x as usize, y as usize) {
+    let pixmap = RecursiveImage::draw(&brush, 5);
+    let pixel = Pixmap::new(3, 3, true);
+    let canvas = Canvas::new(&pixmap, &pixel);
+    
+    let img = ImageBuffer::from_fn(canvas.width as u32, canvas.height as u32, |x, y| {
+        if canvas.get(x as usize, y as usize) {
             image::Luma([0u8])
         } else {
             image::Luma([255u8])
