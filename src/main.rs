1425334c008d175a06b8ece8a1d5a11d49690b9b
Jonny Dee <jonny.dee@gmx.net>
2015-08-08 21:26:13 +0200

    Save as PNG image.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate bit_vec;
-use bit_vec::BitVec;
+extern crate image;
 
-#[derive(Clone)]
+use bit_vec::BitVec;
+use image::{ GenericImage, ImageBuffer };
+use std::fs::File;
+
+
 struct Pixel {
     width : usize,
     height : usize,
@@ -103,10 +107,7 @@ impl Pixmap {
     
     fn get(&self, x : usize, y : usize) -> bool {
         let offset = y*self.width + x;
-        match self.buffer.get(offset) {
-            Some(flag) => flag,
-            None => false
-        }
+        self.buffer.get(offset).unwrap()
     }
 }
 
@@ -157,24 +158,38 @@ impl RecursiveImage {
 
 fn main() {
     let icon = {
-        let mut icon = Pixmap::new(3, 3);
-        icon.set(1, 0, true);
-        icon.set(0, 1, true);
-        icon.set(1, 1, true);
-        icon.set(2, 1, true);
-        icon.set(0, 2, true);
-        icon.set(2, 2, true);
-        icon
+        let mut i = Pixmap::new(7, 7);
+        i.set(0, 0, false); i.set(1, 0, false); i.set(2, 0, false); i.set(3, 0, false); i.set(4, 0, false); i.set(5, 0, false); i.set(6, 0, false);
+        i.set(0, 1, true); i.set(1, 1, true); i.set(2, 1, false); i.set(3, 1, false); i.set(4, 1, true); i.set(5, 1, true); i.set(6, 1, true);
+        i.set(0, 2, true); i.set(1, 2, false); i.set(2, 2, true); i.set(3, 2, false); i.set(4, 2, false); i.set(5, 2, false); i.set(6, 2, true);
+        i.set(0, 3, true); i.set(1, 3, false); i.set(2, 3, true); i.set(3, 3, false); i.set(4, 3, false); i.set(5, 3, false); i.set(6, 3, true);
+        i.set(0, 4, true); i.set(1, 4, false); i.set(2, 4, true); i.set(3, 4, false); i.set(4, 4, true); i.set(5, 4, false); i.set(6, 4, true);
+        i.set(0, 5, true); i.set(1, 5, true); i.set(2, 5, false); i.set(3, 5, false); i.set(4, 5, false); i.set(5, 5, true); i.set(6, 5, false);
+        i.set(0, 6, false); i.set(1, 6, false); i.set(2, 6, false); i.set(3, 6, false); i.set(4, 6, false); i.set(5, 6, false); i.set(6, 6, false);
+        i
     };
         
-    let image = RecursiveImage::draw(icon, 3);
+    let pixmap = RecursiveImage::draw(icon, 5);
     
+    let img = ImageBuffer::from_fn(pixmap.width as u32, pixmap.height as u32, |x, y| {
+        if pixmap.get(x as usize, y as usize) {
+            image::Luma([0u8])
+        } else {
+            image::Luma([255u8])
+        }
+    });
+    
+//    let ref mut out = File::create("out.png").unwrap();
+    let _ = img.save("out.png").ok().expect("Saving image failed");
+
+    /*
     let pixel = {
         let mut pixel = Pixel::new(3, 3, 'O');
         pixel.set_dot(1, 1, ' ');
         pixel
     };
         
-    let canvas = Canvas::new(image, pixel);
+    let canvas = Canvas::new(pixmap, pixel);
     canvas.print();
+    */
 }
