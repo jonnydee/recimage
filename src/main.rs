2e6fa968aaaa8cfc4fe365419609be509697ec1a
Jonny Dee <jonny.dee@gmx.net>
2015-08-25 21:24:13 +0200

    Add version command line parameters.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ use image::{ ImageBuffer };
 use std::env;
 use recursive_image::{ Data, Canvas, Pixmap };
 
+const AUTHOR: &'static str = "Jonny Dee <jonny.dee@gmx.net>";
+const COPYRIGHT_YEARS: &'static str = "2015";
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     let program = args[0].clone();
@@ -16,6 +21,8 @@ fn main() {
     opts.optopt("d", "depth", "set recursion depth", "DEPTH");
     opts.optopt("o", "output", "set output file name", "NAME");
     opts.optopt("p", "pixel", "set pixel file name", "NAME");
+    opts.optflag("v", "", "print version number");
+    opts.optflag("", "version", "print full version information");
     
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => { m }
@@ -27,6 +34,14 @@ fn main() {
         return;
     }
     
+    if matches.opt_present("v") {
+        print_version(&program, false);
+        return;
+    } else if matches.opt_present("version") {
+        print_version(&program, true);
+        return;
+    }
+        
     let input_file_name = if !matches.free.is_empty() {
         matches.free[0].clone()
     } else {
@@ -63,6 +78,14 @@ fn main() {
 fn print_usage(program: &str, opts: Options) {
     let brief = format!("Usage: {} [options] FILE", program);
     print!("{}", opts.usage(&brief));
+}
+
+fn print_version(program: &str, full : bool) {
+    if full {
+        println!("{} v{} ©{} {}", program, VERSION, COPYRIGHT_YEARS, AUTHOR);
+    } else {
+        println!("{}", VERSION);
+    }
 }
 
 fn make_image(file_name : &str, data : &Data, depth : usize) {
