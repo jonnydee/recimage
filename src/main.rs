8b25310d52ff8970051291146160602ac1f88e04
Jonny Dee <jonny.dee@gmx.net>
2015-08-25 20:47:33 +0200

    Add command line parameters.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,70 @@
 extern crate image;
 extern crate recursive_image;
+extern crate getopts;
 
+use getopts::Options;
 use image::{ ImageBuffer };
-use std::fs::File;
-use std::path::Path;
+use std::env;
+//use std::fs::File;
+//use std::path::Path;
 use recursive_image::{ Data, Canvas, Pixmap };
 
 fn main() {
-    let pixel = Pixmap::from_file("pixel.txt").unwrap();
-    let brush = Pixmap::from_file("brush.txt").unwrap();
-    let canvas = Canvas::new(pixel, brush);
-    let depth = 2;
-    make_image("out.png", &canvas, depth);
+    let args: Vec<_> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "print this help menu");
+    opts.optopt("d", "depth", "set recursion depth", "DEPTH");
+    opts.optopt("o", "output", "set output file name", "NAME");
+    opts.optopt("p", "pixel", "set pixel file name", "NAME");
+    
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!(f.to_string()) }
+    };
+    
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    
+    let input_file_name = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+    
+    let depth = match matches.opt_str("d") {
+        Some(depth) => depth.parse::<usize>().unwrap(),
+        None => 0
+    };
+    
+    let output_file_name = match matches.opt_str("o") {
+        Some(file) => file,
+        None => input_file_name.clone() + ".png"
+    };
+    
+    let pixel_file_name = match matches.opt_str("p") {
+        Some(file) => file,
+        None => String::new()
+    };
+        
+    if !pixel_file_name.is_empty() {
+        let pixel = Pixmap::from_file(&pixel_file_name).unwrap();
+        let brush = Pixmap::from_file(&input_file_name).unwrap();
+        let canvas = Canvas::new(pixel, brush);
+        make_image(&output_file_name, &canvas, depth);
+    } else {
+        let brush = Pixmap::from_file(&input_file_name).unwrap();
+        make_image(&output_file_name, &brush, depth);
+    }
+}
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options] FILE", program);
+    print!("{}", opts.usage(&brief));
 }
 
 fn make_image(file_name : &str, data : &Data, depth : usize) {
@@ -24,7 +77,5 @@ fn make_image(file_name : &str, data : &Data, depth : usize) {
         }
     });
     
-    let ref mut fout = File::create(&Path::new(file_name)).unwrap();
-    let _ = image::ImageLuma8(img).save(fout, image::PNG).ok().expect("Saving image failed");
-//	let _ = img.save("out.png").ok().expect("Saving image failed");
+    img.save(file_name).ok().expect("Saving image failed");
 }
