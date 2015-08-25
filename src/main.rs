c02406fea9cbf26de13355d2fea581446f1e78c7
Jonny Dee <jonny.dee@gmx.net>
2015-08-25 21:47:29 +0200

    Add license information.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+// The MIT License (MIT)
+// =====================
+//
+// Copyright © 2015 Johann Duscher (a.k.a. Jonny Dee)
+// 
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the “Software”), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
 extern crate getopts;
 extern crate image;
 extern crate recursive_image;
@@ -82,7 +108,7 @@ fn print_usage(program: &str, opts: Options) {
 
 fn print_version(program: &str, full : bool) {
     if full {
-        println!("{} v{} ©{} {}", program, VERSION, COPYRIGHT_YEARS, AUTHOR);
+        println!("{} v{} - Copyright (c) {} {}", program, VERSION, COPYRIGHT_YEARS, AUTHOR);
     } else {
         println!("{}", VERSION);
     }
